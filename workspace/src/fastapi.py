e8b37cec8c38cfabe440319295556a6fe5d6382e
abhishek9sharma <17585931+abhishek9sharma@users.noreply.github.com>
2025-11-05 22:59:03 +0800

    try scaling replicas vs rps

--- workspace/src/fastapi.py
+++ workspace/src/fastapi.py
@@ -3,28 +3,28 @@ from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
 
-import math
-import random
+# import math
+# import random
 
 
-def compute_intensive_task(n: int = None) -> int:
-    """A CPU-intensive task (calculating primes up to n). If n is not provided, it is randomly chosen."""
-    if n is None:
-        n = random.randint(1000, 10000)  # Randomly choose n between 1000 and 1,000,000
+# def compute_intensive_task(n: int = None) -> int:
+#     """A CPU-intensive task (calculating primes up to n). If n is not provided, it is randomly chosen."""
+#     if n is None:
+#         n = random.randint(1000, 10000)  # Randomly choose n between 1000 and 1,000,000
 
-    primes = []
-    for num in range(2, n + 1):
-        is_prime = True
-        for i in range(2, int(math.sqrt(num)) + 1):
-            if num % i == 0:
-                is_prime = False
-                break
-        if is_prime:
-            primes.append(num)
-    return len(primes)
+#     primes = []
+#     for num in range(2, n + 1):
+#         is_prime = True
+#         for i in range(2, int(math.sqrt(num)) + 1):
+#             if num % i == 0:
+#                 is_prime = False
+#                 break
+#         if is_prime:
+#             primes.append(num)
+#     return len(primes)
 
 
 @app.get("/hello")
 def hello():
-    result = compute_intensive_task()  # This will use CPU
-    return {"message": "Hello, World!", "primes_count": str(result)}
+    #result = compute_intensive_task()  # This will use CPU
+    return {"message": "Hello, World!"}#, "primes_count": str(result)}
